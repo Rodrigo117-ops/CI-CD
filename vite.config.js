d98b31a9c6ec6f3bcee10c75233ca7711c8582de
YenKG <botaumacoisamaiscriativa@gmail.com>
2025-11-14 09:16:01 -0300

    Add GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // Caminho base para GitHub Pages:
+  base: '/CI-CD/',
+
   plugins: [react()],
   test: {
     environment: 'jsdom',
