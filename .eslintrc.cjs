465b22161dd06a12febd74ff15dc86ec2f569792
YenKG <botaumacoisamaiscriativa@gmail.com>
2025-11-16 19:58:38 -0300

    Fix: ESLint configurado para testes Vitest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,34 @@
+// .eslintrc.cjs
 module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
   ],
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: 'detect',
+    },
   },
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react', 'react-hooks'],
   rules: {
-    'react/react-in-jsx-scope': 'off'
-  }
-}
+    'react/react-in-jsx-scope': 'off',
+  },
+  globals: {
+    // Adicionamos as globals dos testes aqui 👇
+    describe: 'readonly',
+    it: 'readonly',
+    test: 'readonly',
+    expect: 'readonly',
+    beforeEach: 'readonly',
+    afterEach: 'readonly',
+  },
+};
